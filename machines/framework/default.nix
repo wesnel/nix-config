c0ac290de704ae831c7da95ce3bf5fb605149ff7
Wesley Nelson <wgn@wesnel.dev>
2022-09-15 19:58:28 -0700

    Disable ipfs

--- machines/framework/default.nix
+++ machines/framework/default.nix
@@ -14,7 +14,6 @@ inputs@
     ../../modules/programs/steam
     ../../modules/services/fwupd
     ../../modules/services/interception-tools
-    ../../modules/services/ipfs
     ../../modules/services/mullvad-vpn
     ../../modules/services/pcscd
     ../../modules/services/printing
