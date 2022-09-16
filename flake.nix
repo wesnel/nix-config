1dc58677aa22b1634102a78bb8193d0b6959800b
Wesley Nelson <wgn@wesnel.dev>
2022-09-15 19:57:52 -0700

    Remove quotes from input urls

--- flake.nix
+++ flake.nix
@@ -3,31 +3,31 @@
 
   inputs = {
     darwin = {
-      url = "github:lnl7/nix-darwin";
+      url = github:lnl7/nix-darwin;
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     emacs-overlay = {
-      url = "github:nix-community/emacs-overlay";
+      url = github:nix-community/emacs-overlay;
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = github:nix-community/home-manager;
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     nixos-hardware = {
-      url = "github:nixos/nixos-hardware";
+      url = github:nixos/nixos-hardware;
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     nixpkgs = {
-      url = "github:nixos/nixpkgs/nixos-unstable";
+      url = github:nixos/nixpkgs/nixos-unstable;
     };
 
     nur = {
-      url = "github:nix-community/NUR";
+      url = github:nix-community/NUR;
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
