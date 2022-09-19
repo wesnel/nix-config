a9b637b055d3e96862b56900ff5c91b2cd47c7f6
Wesley Nelson <wgn@wesnel.dev>
2022-09-19 15:00:32 -0700

    Add alias to launch editor

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -35,12 +35,14 @@ inputs@
     };
 
     shellAliases = let
+      e = "$EDITOR";
       ls = "${pkgs.exa}/bin/exa";
       l = "${ls} -laF --color=always --color-scale --icons --git";
       tree = "${ls} -T";
       t = "${l} -T";
     in {
       inherit
+        e
         ls
         l
         tree
