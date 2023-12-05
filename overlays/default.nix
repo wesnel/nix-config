b87e80e746bd60a3cd18d57f2d2d9b3e17f224a7
Wesley Nelson <wgn@wesnel.dev>
2023-12-04 16:17:15 -0800

    Add overlay for installing llava
    Does not currently work on my work-provided Mac due to CrowdStrike.

--- overlays/default.nix
+++ overlays/default.nix
@@ -87,5 +87,29 @@
 
         meta.broken = builtins.compareVersions qt.qtbase.version "5.15" < 0;
       };
+
+      # FIXME: This is blocked by CrowdStrike on my work laptop :(
+      # https://github.com/Mozilla-Ocho/llamafile/issues/14
+      llava = final.stdenv.mkDerivation rec {
+        pname = "llava";
+        version = "v1.5-7b-q4";
+
+        src = final.fetchurl {
+          url = "https://huggingface.co/jartine/${pname}-v1.5-7B-GGUF/resolve/main/${pname}-${version}-server.llamafile";
+          hash = "sha256-O+6UYMSTsi/5wbPTP0rbwRY65Xuz6GtVZ/g7SyGE5lI=";
+        };
+
+        dontUnpack = true;
+
+        installPhase = ''
+          runHook preInstall
+
+          mkdir -p $out/bin
+          cp $src $out/bin/${pname}-${version}-server.llamafile
+          chmod 755 $out/bin/${pname}-${version}-server.llamafile
+
+          runHook postInstall
+        '';
+      };
     })
 ]
