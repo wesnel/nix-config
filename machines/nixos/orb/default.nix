6ab2dbc9d9a0eca04886c99f8dad41f52d8fda97
Wesley Nelson <wgn@wesnel.dev>
2024-05-10 12:23:14 -0500

    Remove some unusable modules from the virtual machine

--- machines/nixos/orb/default.nix
+++ machines/nixos/orb/default.nix
@@ -11,13 +11,9 @@ let
     emacs-config.nixosModules.home
 
     ../../../modules/home-manager/emacs
-    ../../../modules/home-manager/email
     ../../../modules/home-manager/fish
     ../../../modules/home-manager/git
-    ../../../modules/home-manager/gnupg
     ../../../modules/home-manager/man
-    ../../../modules/home-manager/pass
-    ../../../modules/home-manager/yubikey
   ];
 
   nixosModules = [
