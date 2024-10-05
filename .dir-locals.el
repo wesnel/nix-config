a1c51bd01c24a6275fe1d5b5f5eb6c16baf75289
Wesley Nelson <wgn@wesnel.dev>
2024-10-04 17:27:31 -0700

    Configure Emacs code formatter for Nix files in this project

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nix-mode . ((eglot-workspace-configuration . (:nil (:formatting (:command ["alejandra"])))))))
