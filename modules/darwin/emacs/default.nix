f7ccf82ee3351d9b5e5891272dce8e784550a490
Wesley Nelson <wgn@wgn.dev>
2025-07-08 12:47:00 -0700

    Move darwin-specific script to darwin-specific module

--- modules/darwin/emacs/default.nix
+++ modules/darwin/emacs/default.nix
@@ -6,6 +6,12 @@
 }:
 with lib; let
   cfg = config.wgn.darwin.emacs;
+
+  openNewEmacsAppScriptName = "open-new-emacs-app";
+
+  openNewEmacsApp = pkgs.writeShellScriptBin openNewEmacsAppScriptName ''
+    open -a "/Applications/Nix Apps/Emacs.app" -nW --args --chdir $PWD --no-splash "$@"
+  '';
 in {
   options.wgn.darwin.emacs = {
     enable = mkEnableOption "Enables my Emacs setup for Darwin";
@@ -14,12 +20,26 @@ in {
   config = mkIf cfg.enable {
     environment.systemPackages = [
       config.programs.wgn.emacs.package
+      openNewEmacsApp
     ];
 
     programs = {
+      fish.interactiveShellInit =
+        lib.mkIf config.programs.fish.enable
+        ''
+          # ${openNewEmacsAppScriptName} must be on $PATH in order for platformctl to recognize it as a valid
+          # command.  It opens a completely new GUI Emacs App instance and waits for
+          # it to exit.  I would prefer to open a new frame of the existing GUI Emacs
+          # App instance (if one exists), but I cannot figure this out.  When I try,
+          # I end up either opening a terminal instance of the Emacs client, or
+          # platformctl thinks that the command exits instantly as soon as a new
+          # Emacs buffer is created in the daemon.
+          set -gx VISUAL ${openNewEmacsApp}/bin/${openNewEmacsAppScriptName}
+        '';
+
       wgn.emacs = {
         enable = true;
-        package = mkDefault pkgs.wgn-emacs-macport;
+        package = pkgs.wgn-emacs-macport;
       };
     };
 
