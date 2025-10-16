1dea7b7d5299efb7c1345129ee16f681aa3f0fd9
Wesley Nelson <wgn@wgn.dev>
2025-10-16 13:40:06 -0700

    Add extra environment variables to shell startup

--- modules/darwin/fish/default.nix
+++ modules/darwin/fish/default.nix
@@ -19,6 +19,7 @@ in {
       interactiveShellInit =
         # TODO: Maybe there is a better place to put work-specific config like this.
         # TODO: Use my ep package for fetching password instead of invoking emacs directly.
+        # TODO: Modify ep package to be able to batch-export passwords as environment variables so that we don't load emacs N times on shell startup!
         # TODO: This depends upon certain secrets being available in `~/.authinfo.gpg`, so it is not exactly deterministic.
         mkIf config.wgn.darwin.emacs.enable ''
           set -gx ARTIFACTORY_PYPI_USERNAME ${username}
@@ -31,6 +32,8 @@ in {
           set -gx HF_TOKEN (gpg --card-status > /dev/null && emacs --quick --batch --load 'auth-source' --eval "(condition-case nil (princ (auth-info-password (nth 0 (auth-source-search :host \"huggingface.co\" :user \"${username}@shipt.com\" :max 1)))) (file-error nil))")
           set -gx DD_APP_KEY (gpg --card-status > /dev/null && emacs --quick --batch --load 'auth-source' --eval "(condition-case nil (princ (auth-info-password (nth 0 (auth-source-search :host \"datadoghq.com\" :user \"${username}@shipt.com\" :max 1)))) (file-error nil))")
           set -gx DD_API_KEY (gpg --card-status > /dev/null && emacs --quick --batch --load 'auth-source' --eval "(condition-case nil (princ (auth-info-password (nth 0 (auth-source-search :host \"datadoghq.com\" :user \"shipt\" :max 1)))) (file-error nil))")
+          set -gx SHIPT_USERNAME '${username}'
+          set -gx SHIPT_PASSWORD (gpg --card-status > /dev/null && emacs --quick --batch --load 'auth-source' --eval "(condition-case nil (princ (auth-info-password (nth 0 (auth-source-search :host \"shipt.okta.com\" :user \"${username}@shipt.com\" :max 1)))) (file-error nil))")
         '';
     };
 
