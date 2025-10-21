10dd58c9978cb0210403957272c396a0b8316d45
Wesley Nelson <wgn@wgn.dev>
2025-10-21 04:21:33 -0700

    Attempt to speed up fish shell startup

--- modules/darwin/fish/default.nix
+++ modules/darwin/fish/default.nix
@@ -19,21 +19,45 @@ in {
       interactiveShellInit =
         # TODO: Maybe there is a better place to put work-specific config like this.
         # TODO: Use my ep package for fetching password instead of invoking emacs directly.
-        # TODO: Modify ep package to be able to batch-export passwords as environment variables so that we don't load emacs N times on shell startup!
+        # TODO: Modify ep package to be able to batch-export passwords as environment variables so that we don't need this crazy script.
         # TODO: This depends upon certain secrets being available in `~/.authinfo.gpg`, so it is not exactly deterministic.
         mkIf config.wgn.darwin.emacs.enable ''
           set -gx ARTIFACTORY_PYPI_USERNAME ${username}
-          set -gx ARTIFACTORY_PYPI_PASSWORD (gpg --card-status > /dev/null && emacs --quick --batch --load 'auth-source' --eval "(condition-case nil (princ (auth-info-password (nth 0 (auth-source-search :host \"artifactory.gcp.shipttech.com\" :user \"$ARTIFACTORY_PYPI_USERNAME\" :max 1)))) (file-error nil))")
 
+          gpg --card-status > /dev/null
+
+          # Fetch all passwords in one Emacs call, each line corresponds to a password
+          set secrets (emacs --quick --batch --load 'auth-source' --eval "
+            (princ (mapconcat
+              (lambda (entry)
+                (condition-case nil
+                    (auth-info-password
+                     (nth 0 (auth-source-search
+                             :host (car entry)
+                             :user (cdr entry)
+                             :max 1)))
+                  (file-error \"\")))
+              '((\"artifactory.gcp.shipttech.com\" . \"$ARTIFACTORY_PYPI_USERNAME\")
+                (\"github.com\" . \"wesnel\")
+                (\"huggingface.co\" . \"${username}@shipt.com\")
+                (\"datadoghq.com\" . \"${username}@shipt.com\")
+                (\"datadoghq.com\" . \"shipt\")
+                (\"shipt.okta.com\" . \"${username}@shipt.com\"))
+              \" \"))")
+
+          # Split output into individual lines
+          set secrets (echo $secrets | string split ' ')
+
+          # Assign secrets to respective environment variables
+          set -l index 1
+          for var in ARTIFACTORY_PYPI_PASSWORD GITHUB_TOKEN HF_TOKEN DD_APP_KEY DD_API_KEY SHIPT_PASSWORD
+              set -gx $var $secrets[$index]
+              set index (math $index + 1)
+          end
+
+          set -gx SHIPT_USERNAME $ARTIFACTORY_PYPI_USERNAME
           set -gx POETRY_HTTP_BASIC_SHIPT_RESOLVE_USERNAME $ARTIFACTORY_PYPI_USERNAME
           set -gx POETRY_HTTP_BASIC_SHIPT_RESOLVE_PASSWORD $ARTIFACTORY_PYPI_PASSWORD
-
-          set -gx GITHUB_TOKEN (gpg --card-status > /dev/null && emacs --quick --batch --load 'auth-source' --eval "(condition-case nil (princ (auth-info-password (nth 0 (auth-source-search :host \"github.com\" :user \"wesnel\" :max 1)))) (file-error nil))")
-          set -gx HF_TOKEN (gpg --card-status > /dev/null && emacs --quick --batch --load 'auth-source' --eval "(condition-case nil (princ (auth-info-password (nth 0 (auth-source-search :host \"huggingface.co\" :user \"${username}@shipt.com\" :max 1)))) (file-error nil))")
-          set -gx DD_APP_KEY (gpg --card-status > /dev/null && emacs --quick --batch --load 'auth-source' --eval "(condition-case nil (princ (auth-info-password (nth 0 (auth-source-search :host \"datadoghq.com\" :user \"${username}@shipt.com\" :max 1)))) (file-error nil))")
-          set -gx DD_API_KEY (gpg --card-status > /dev/null && emacs --quick --batch --load 'auth-source' --eval "(condition-case nil (princ (auth-info-password (nth 0 (auth-source-search :host \"datadoghq.com\" :user \"shipt\" :max 1)))) (file-error nil))")
-          set -gx SHIPT_USERNAME '${username}'
-          set -gx SHIPT_PASSWORD (gpg --card-status > /dev/null && emacs --quick --batch --load 'auth-source' --eval "(condition-case nil (princ (auth-info-password (nth 0 (auth-source-search :host \"shipt.okta.com\" :user \"${username}@shipt.com\" :max 1)))) (file-error nil))")
         '';
     };
 
