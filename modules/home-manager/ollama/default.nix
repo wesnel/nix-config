8bdf16615233c6f679c6c40e244fb07dc1c955f3
Wesley Nelson <wgn@wgn.dev>
2026-02-22 01:15:28 -0800

    Remove AIChat

--- modules/home-manager/ollama/default.nix
+++ modules/home-manager/ollama/default.nix
@@ -15,29 +15,5 @@ in {
     services.ollama = {
       enable = true;
     };
-
-    programs.aichat = {
-      enable = true;
-
-      settings = {
-        model = "ollama:mistral-small3.1:latest";
-
-        clients = [
-          {
-            type = "openai-compatible";
-            name = "ollama";
-            api_base = "http://${config.services.ollama.host}:${builtins.toString config.services.ollama.port}/v1";
-            # TODO: Add more models here depending on what's installed.
-            models = [
-              {
-                name = "mistral-small3.1:latest";
-                supports_function_calling = true;
-                supports_vision = true;
-              }
-            ];
-          }
-        ];
-      };
-    };
   };
 }
