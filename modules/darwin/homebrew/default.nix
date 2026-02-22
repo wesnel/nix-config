5f58213443554d84b37528affa9e48a08d0b565b
Wesley Nelson <wgn@wgn.dev>
2026-02-21 21:32:49 -0800

    Install stats via homebrew cask managed by nix

--- modules/darwin/homebrew/default.nix
+++ modules/darwin/homebrew/default.nix
@@ -7,19 +7,18 @@ with lib; let
   cfg = config.wgn.darwin.homebrew;
 in {
   options.wgn.darwin.homebrew = {
+    # HACK: Homebrew itself needs to be installed outside of Nix.
     enable = mkEnableOption "Enables my Homebrew setup for Darwin";
   };
 
   config = mkIf cfg.enable {
-    # TODO: nix-darwin has more options for homebrew which should be used.
+    homebrew = {
+      enable = true;
+      enableFishIntegration = config.programs.fish.enable;
 
-    programs = {
-      fish.interactiveShellInit =
-        # HACK: I think Homebrew needs to be installed outside of Nix?
-        lib.mkIf config.programs.fish.enable
-        ''
-          fish_add_path /opt/homebrew/bin
-        '';
+      casks = [
+        "stats"
+      ];
     };
   };
 }
